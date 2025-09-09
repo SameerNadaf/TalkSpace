e9a634188d7226e2026b8fc89a2937f21beca814
Sameer Nadaf <nsameernadaf@gmail.com>
2025-09-09 19:32:16 +0530

    Add to remote

--- TalkSpace/App/TalkSpaceApp.swift
+++ TalkSpace/App/TalkSpaceApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct TalkSpaceApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
---
+++ TalkSpace/Features/Home/Views/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Message
+//
+//  Created by Sameer  on 09/09/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    HomeView()
+}
