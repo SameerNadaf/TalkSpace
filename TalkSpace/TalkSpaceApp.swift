eb5d630302af319ed93bfed2ef43a22a57caa774
Sameer Nadaf <nsameernadaf@gmail.com>
2025-09-09 19:29:48 +0530

    Initial Commit

---
+++ TalkSpace/TalkSpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  TalkSpaceApp.swift
+//  TalkSpace
+//
+//  Created by Sameer  on 09/09/25.
+//
+
+import SwiftUI
+
+@main
+struct TalkSpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
